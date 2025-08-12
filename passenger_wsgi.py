1281f11efb432d416100980d91cd68d555cc71e3
Jonah Onah <onahjonah@gmail.com>
2025-08-12 23:15:31 +0100

    updated passenger settings

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-
+# Add your project directory to sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
+# Set the Django settings module for production
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tlng_summit_project.settings_production')
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+# Import Django WSGI application
+from django.core.wsgi import get_wsgi_application
+
+# Create the WSGI application
+application = get_wsgi_application()
